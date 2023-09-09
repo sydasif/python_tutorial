4b1e5e68fd67af7fcf5a5a94b8f635da6c228b5b
sydasif <sydasif78@gmail.com>
2023-09-09 16:57:30 +0500

    rename

--- my_class_01.py
+++ my_class_01.py
@@ -4,27 +4,19 @@
 class Employee:
     """Employee Class"""
 
-    num_of_emp = 0
-    raise_amount = 1.04
-
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
         self.pay = pay
         self.email = f"{first}.{last}@compny.com"
 
-        Employee.num_of_emp += 1
-
     def full_name(self):
         """Print Full Name"""
         return f"{self.first} {self.last}"
-
-    def applay_raise(self):
-        """Raise Employee Pay"""
-        self.pay = int(self.pay * self.raise_amount)
 
 
 emp_1 = Employee("Jan", "Woo", 5000)
 emp_2 = Employee("Alex", "Core", 6000)
 
-print(Employee.num_of_emp)
+print(emp_1.full_name())
+print(emp_1.pay)
