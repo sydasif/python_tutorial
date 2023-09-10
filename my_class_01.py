644072ca6554e95255b8eb0b75ee292d0728e00c
sydasif <sydasif78@gmail.com>
2023-09-10 19:45:05 +0500

    Add commits in code

--- my_class_01.py
+++ my_class_01.py
@@ -4,19 +4,19 @@
 class Employee:
     """Employee Class"""
 
-    def __init__(self, first, last, pay):
+    def __init__(self, first, last, pay):  # Attributes of Employee class
         self.first = first
         self.last = last
         self.pay = pay
         self.email = f"{first}.{last}@compny.com"
 
-    def full_name(self):
+    def full_name(self):  # Class method
         """Print Full Name"""
         return f"{self.first} {self.last}"
 
 
-emp_1 = Employee("Jan", "Woo", 5000)
-emp_2 = Employee("Alex", "Core", 6000)
+emp_1 = Employee("Jan", "Woo", 5000)  # instance of a class
+emp_2 = Employee("Alex", "Core", 6000)  # instance of a class
 
 print(emp_1.full_name())
 print(emp_1.pay)
